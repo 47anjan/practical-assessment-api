a5df7317ced2cafff162ad7a01a88bdfe4735f51
47anjan <anjankarmakar15@gmail.com>
2025-07-08 02:21:29 +0600

    update env

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT, () => {
+    app.listen(5000, () => {
       console.log("Server is running on port 5000");
     });
   })
