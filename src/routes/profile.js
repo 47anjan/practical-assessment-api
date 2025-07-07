27eb09935d147ba2251ed39a366368307fcc6c08
47anjan <anjankarmakar15@gmail.com>
2025-07-07 16:55:44 +0600

    add cart model

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -14,7 +14,7 @@ router.get("/profile/view", authorized, async (req, res) => {
 
     res.send(userWithoutPassword);
   } catch (err) {
-    return res.status(401).send({ message: "Unauthorized" });
+    return res.status(401).send({ message: err });
   }
 });
 
